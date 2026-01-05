c1cf2de665381a6114dd44f41bdd02e843629977
Egor Kochetov <egorkochetov02@gmail.com>
2026-01-05 17:43:59 +0500

    Resume is now shown on the page

--- backend/app/app.go
+++ backend/app/app.go
@@ -25,6 +25,8 @@ func testJSONDecoder(resume latex.Resume) error {
 	return nil
 }
 
+const frontendServerAddr = "http://localhost:5100"
+
 const pdfDir = "pdf_resume"
 
 const basePDFname = "resume"
@@ -39,7 +41,13 @@ func (a *App) mainPage(w http.ResponseWriter, r *http.Request) {
 // Return user's resume in pdf format
 func (a *App) getResume(w http.ResponseWriter, r *http.Request) {
 	resume := &latex.ResumeClassic{}
-	err := testJSONDecoder(resume)
+	/*err := testJSONDecoder(resume)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "error decoding test JSON: %v\n", err)
+		return
+	}*/
+	err := json.NewDecoder(r.Body).Decode(resume)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "error decoding test JSON: %v\n", err)
@@ -76,12 +84,43 @@ func (a *App) getResume(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (a *App) debugPage(w http.ResponseWriter, r *http.Request) {
+	resume := &latex.ResumeClassic{}
+	err := json.NewDecoder(r.Body).Decode(resume)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Printf("error decoding test JSON: %v\n", err)
+		return
+	}
+	data, _ := json.MarshalIndent(resume, " ", "  ")
+	fmt.Printf("%s\n", data)
+}
+
+// For allowing CORS
+func allowCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", frontendServerAddr)
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			w.Header().Set("Access-Control-Allow-Methods", "POST")
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Starts server
 func (a *App) Run() error {
 	http.HandleFunc("/", a.mainPage)
 	http.HandleFunc("/resume", a.getResume)
+	http.HandleFunc("/debug", a.debugPage)
 
 	server := &http.Server{
 		Addr:              ":8100",
+		Handler:           allowCORS(http.DefaultServeMux),
 		ReadTimeout:       30 * time.Second,
 		ReadHeaderTimeout: 15 * time.Second,
 		WriteTimeout:      30 * time.Second,
--- frontend/static/js/sending_classic.js
+++ frontend/static/js/sending_classic.js
@@ -1,7 +1,35 @@
 "use strict"
 
-function debugObject() {
+/*function debugObject() {
     console.log(createObject(resume));
+}*/
+
+async function sendResume() {
+    const response = await fetch("http://localhost:8100/resume", {
+        method: "POST",
+        body: JSON.stringify(createObject(resume))
+    })
+    if (response.ok) {
+        console.log("ok")
+    } else {
+        console.log("not ok")
+    }
+    return response
 }
 
-sendResume.addEventListener("click", debugObject);
+function previewPDF(response) {
+    console.log(response)
+}
+
+async function buttonGeneratePDF() {
+    const response = await sendResume()
+    const PDFblob = await response.blob()
+    console.log(PDFblob)
+    pdfViewNode.src = URL.createObjectURL(PDFblob)
+}
+
+const pdfViewNode = document.getElementById("pdfViewNode")
+
+buttonSendResume.addEventListener("click", buttonGeneratePDF);
+console.log(25)
+//buttonSendResume.addEventListener("click", debugObject);
